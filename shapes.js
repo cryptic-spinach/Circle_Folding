fe5162b4c33b78b01e52810b669d82e1e3da386a
Your Name <bcreynolds2@gmail.com>
2019-07-31 18:43:10 -0400

    Initial commit.

---
+++ shapes.js
@@ -0,0 +1,43 @@
+function greenCircle(x, y) {
+  stroke(77, 255, 50);
+  fill(0, 0, 0, 0);
+  ellipse(x, y, 500);
+}
+
+function purpleDot(x, y) {
+	stroke(153,50,204);
+	fill(153,50,204);
+	ellipse(x, y, 7.5);
+}
+
+function chord(x1, y1, m, r) {
+  var b = -m*x1 + y1;
+  var d = Math.pow(m, 2) + 1;
+  var e = 2*b*m;
+  var f = Math.pow(b, 2) - Math.pow(r, 2);
+  var pos_x = (-e + Math.sqrt(Math.pow(e, 2) - 4*d*f)) / (2*d);
+  var pos_y = m * pos_x + b;
+  var neg_x = (-e - Math.sqrt(Math.pow(e, 2) - 4*d*f)) / (2*d);
+  var neg_y = m * neg_x + b;
+  stroke(70, 179, 181);
+  line(pos_x, pos_y, neg_x, neg_y);
+}
+
+class Bubble {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.brightness = 255;
+  }
+
+  show() {
+    noStroke();
+
+    fill(0);
+    ellipse(this.x, this.y, this.r);
+
+    fill(255,165,0, this.brightness);
+    ellipse(this.x, this.y, this.r);
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,50 @@
+var r = 250;
+var px = 0;
+var py = 200;
+var makeBubs = true;
+var bubbles = [];
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	translate(windowWidth/2, windowHeight/2);
+	rotate(Math.PI/2);
+	slider = createSlider(0, Math.PI*2, 0, 0.04);
+	slider.position(20, 20);
+}
+
+function draw() {
+	translate(windowWidth/2, windowHeight/2);
+	rotate(Math.PI/2);
+	background(0);
+
+	purpleDot(0, 0);
+	purpleDot(px, py);
+
+	var xcoord = r * Math.sin(slider.value());
+	var ycoord = r * Math.cos(slider.value());
+
+	var midx = (xcoord + px)/2;
+	var midy = (ycoord + py)/2;
+
+	if (makeBubs == true) {
+		var b1 = new Bubble(xcoord, ycoord, 8.5);
+		var b2 = new Bubble(midx, midy, 8.5);
+		bubbles.push(b1);
+		bubbles.push(b2);
+	}
+
+	makeBubs = false;
+
+	var perpslope = -(px - xcoord)/(py - ycoord);
+
+	greenCircle(0, 0);
+	chord(midx, midy, perpslope, r);
+
+	bubbles[0].x = xcoord;
+	bubbles[0].y = ycoord;
+	bubbles[0].show();
+
+	bubbles[1].x = midx;
+	bubbles[1].y = midy;
+	bubbles[1].show();
+}
