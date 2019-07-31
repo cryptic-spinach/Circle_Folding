97495f925dba3edf2ab2a18d2809528ccaae9172
Your Name <bcreynolds2@gmail.com>
2019-07-31 19:38:22 -0400

    Add chord trails.

--- sketch.js
+++ sketch.js
@@ -5,12 +5,17 @@ var makeBubs = true;
 var bubbles = [];
 var theta = 0;
 var interval_track = 0;
-var theta_speed = 0.04
+var theta_speed = 0.3;
+
+var midx_memory;
+var midy_memory;
+var perpslope_memory;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	translate(windowWidth/2, windowHeight/2);
 	rotate(Math.PI/2);
+	background(0);
 	// slider = createSlider(0, Math.PI*2, 0, 0.04);
 	// slider.position(20, 20);
 }
@@ -20,7 +25,6 @@ function draw() {
 
 	translate(windowWidth/2, windowHeight/2);
 	rotate(Math.PI/2);
-	background(0);
 
 	purpleDot(0, 0);
 	purpleDot(px, py);
@@ -42,15 +46,25 @@ function draw() {
 
 	var perpslope = -(px - xcoord)/(py - ycoord);
 
+	midx_memory = midx;
+	midy_memory = midy;
+	perpslope_memory = perpslope;
+
 	greenCircle(0, 0);
 
 	chord(midx, midy, perpslope, r);
 
-	bubbles[0].x = xcoord;
-	bubbles[0].y = ycoord;
-	bubbles[0].show();
+	// bubbles[0].x = xcoord;
+	// bubbles[0].y = ycoord;
+	// bubbles[0].show();
+	//
+	// bubbles[1].x = midx;
+	// bubbles[1].y = midy;
+	// bubbles[1].show();
 
-	bubbles[1].x = midx;
-	bubbles[1].y = midy;
-	bubbles[1].show();
+}
+
+
+function mousePressed () {
+	background(0);
 }
