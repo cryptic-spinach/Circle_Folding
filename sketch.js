ba4a95049efd885d493ce6cc298981ac1c9111d4
Your Name <bcreynolds2@gmail.com>
2019-07-31 19:13:52 -0400

    Change title tag to Ellipse_from_chords. Make revolving chord autonomous.

--- sketch.js
+++ sketch.js
@@ -3,16 +3,21 @@ var px = 0;
 var py = 200;
 var makeBubs = true;
 var bubbles = [];
+var theta = 0;
+var interval_track = 0;
+var theta_speed = 0.04
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	translate(windowWidth/2, windowHeight/2);
 	rotate(Math.PI/2);
-	slider = createSlider(0, Math.PI*2, 0, 0.04);
-	slider.position(20, 20);
+	// slider = createSlider(0, Math.PI*2, 0, 0.04);
+	// slider.position(20, 20);
 }
 
 function draw() {
+	theta += theta_speed;
+
 	translate(windowWidth/2, windowHeight/2);
 	rotate(Math.PI/2);
 	background(0);
@@ -20,8 +25,8 @@ function draw() {
 	purpleDot(0, 0);
 	purpleDot(px, py);
 
-	var xcoord = r * Math.sin(slider.value());
-	var ycoord = r * Math.cos(slider.value());
+	var xcoord = r * Math.sin(theta);
+	var ycoord = r * Math.cos(theta);
 
 	var midx = (xcoord + px)/2;
 	var midy = (ycoord + py)/2;
@@ -38,6 +43,7 @@ function draw() {
 	var perpslope = -(px - xcoord)/(py - ycoord);
 
 	greenCircle(0, 0);
+
 	chord(midx, midy, perpslope, r);
 
 	bubbles[0].x = xcoord;
