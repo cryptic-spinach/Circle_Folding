5208c18bc15f6f9287a14336107051345a9ac17a
Your Name <bcreynolds2@gmail.com>
2019-08-08 14:13:49 -0400

    Revert dom styles to default.

--- dom_objects.js
+++ dom_objects.js
@@ -8,22 +8,13 @@ function dom_init() {
   clear_button.position(20, 20);
   clear_button.mousePressed(clearChordTrails);
   clear_button.attribute('disabled', 'true');
-  clear_button.style('background-color', '#4CAF50');
-  clear_button.style('border', 'none');
-  clear_button.style('color', 'white');
 
   toggle_button = createButton('Auto/Manual toggle');
   toggle_button.position(20, 50);
   toggle_button.mousePressed(toggle);
-  toggle_button.style('background-color', '#4CAF50');
-  toggle_button.style('border', 'none');
-  toggle_button.style('color', 'white');
 
   slider = createSlider(0, Math.PI*2, 0, 0.04);
   slider.position(20, 85);
-  slider.style('-webkit-appearance', 'none');
-  slider.style('background-color', 'rgb(80,80,80)');
-  slider.style('border-radius', '5px');
 
   theta_text = createElement('p', 'Theta');
   theta_text.position(200, 50);
